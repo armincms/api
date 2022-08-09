f3c12cc5f40b495949d2a370b25b5ae452d6aa59
Ismail Zare <zarehesmaiel@gmail.com>
2022-08-09 19:27:56 +0430

    fix missing request user bug

--- src/Cypress/Api.php
+++ src/Cypress/Api.php
@@ -3,31 +3,35 @@
 namespace Armincms\Api\Cypress;
 
 use Laravel\Nova\Exceptions\AuthenticationException;
-use Zareismail\Cypress\Component; 
+use Zareismail\Cypress\Component;
 use Zareismail\Cypress\Contracts\Resolvable;
 use Zareismail\Cypress\Http\Requests\CypressRequest;
 
 class Api extends Component implements Resolvable
-{    
+{
     /**
      * The display layout class name.
-     * 
+     *
      * @var string
-     */ 
+     */
     public $layout = \Zareismail\Cypress\Layouts\Clean::class;
-    
+
     /**
      * Resolve the resoruce's value for the given request.
      *
-     * @param  \Zareismail\Cypress\Http\Requests\CypressRequest  $request 
+     * @param  \Zareismail\Cypress\Http\Requests\CypressRequest  $request
      * @return void
      */
     public function resolve($request): bool
-    { 
+    {
         throw_unless(
             \Auth::guard('admin')->check() || \Auth::guard('sanctum')->check(),
             AuthenticationException::class
-        ); 
+        );
+
+        $request->setUserResolver(function() {
+            return \Auth::guard('sanctum')->user();
+        });
 
         return true;
     }
